a7f1c29d8f10872c1df4b47796c8ea009d756165
Baiju012 <namastenaman5@gmail.com>
2024-12-07 19:05:01 +0000

    Updated webiste files from OpenShift workspace

--- app.py
+++ app.py
@@ -14,25 +14,25 @@ def home():
             {"name": "Vikas Mehta", "company": "Tesla", "image": "alumni5.jpg"}
         ],
         "packages": [
-            {"name": "Aryan Gupta", "company": "Google", "image": "package1.jpg"},
-            {"name": "Shreya Das", "company": "Amazon", "image": "package2.jpg"},
+            {"name": "Aryan Gupta", "company": "Google", "image": "pack1.png"},
+            {"name": "Shreya Das", "company": "Amazon", "image": "pack2.png"},
             {"name": "Kunal Jain", "company": "Microsoft", "image": "package3.jpg"},
             {"name": "Niharika Patel", "company": "Adobe", "image": "package4.jpg"},
             {"name": "Manoj Yadav", "company": "Tesla", "image": "package5.jpg"}
         ],
         "gate_scores": [
-            {"rank": 1, "name": "Aman Jain", "image": "pyush.jpg"},
-            {"rank": 2, "name": "Anjali Gupta", "image": "gate2.jpg"},
+            {"rank": 1, "name": "Aman Jain", "image": "Gate1.png"},
+            {"rank": 2, "name": "Anjali Gupta", "image": "gate2.png"},
             {"rank": 5, "name": "Ravi Kumar", "image": "gate3.jpg"},
             {"rank": 8, "name": "Sonal Sharma", "image": "gate4.jpg"},
             {"rank": 10, "name": "Rajiv Singh", "image": "gate5.jpg"}
         ],
         "workshops": [
-            {"topic": "AI & ML", "image": "workshop1.jpg"},
-            {"topic": "Cloud Computing", "image": "workshop2.jpg"},
-            {"topic": "Cybersecurity", "image": "workshop3.jpg"},
-            {"topic": "IoT", "image": "workshop4.jpg"},
-            {"topic": "Blockchain", "image": "workshop5.jpg"}
+            {"topic": "AI & ML", "image": "work1.jpg"},
+            {"topic": "Cloud Computing", "image": "work2.jpg"},
+            {"topic": "Cybersecurity", "image": "work3.jpg"},
+            {"topic": "IoT", "image": "work4.jpg"},
+            {"topic": "Blockchain", "image": "work5.jpg"}
         ]
     }
     return render_template('home.html', data=data)
@@ -59,22 +59,22 @@ def department_detail(department_name):
             {"name": "Ankit Raj", "image": "hod5.jpg"}
         ],
         "ECE": [
-            {"name": "Prof. Vipul Sharma", "image": "Dean_sir.png"},
+            {"name": "Prof. Vipul Sharma (Computational Electromagnetics and Microwaves HOD)", "image": "Dean_sir.png"},
             {"name": "Dr. Tanuj Sir", "image": "Tanuj_sir.png"},
-            {"name": "Rahul Singh", "image": "hod3.jpg"},
+            {"name": "Mr. Anuj Kumar Sharma", "image": "Screenshot 2024-12-07 221926.png"},
             {"name": "Aditi Sharma", "image": "hod4.jpg"},
             {"name": "Ankit Raj", "image": "hod5.jpg"}
         ],
         "EE": [
-            {"name": "Mayank Aggrawal", "image": "hod1.jpg"},
-            {"name": "Suyash Gupta", "image": "hod2.jpg"},
+            {"name": "Mr. Gajendra Singh Rawat", "image": "Gajendra.png"},
+            {"name": "Dr. Brijesh Kumar", "image": "Screenshot 2024-12-07 222143.png"},
             {"name": "Rahul Singh", "image": "hod3.jpg"},
             {"name": "Aditi Sharma", "image": "hod4.jpg"},
             {"name": "Ankit Raj", "image": "hod5.jpg"}
         ],
         "ME": [
-            {"name": "Mayank Aggrawal", "image": "hod1.jpg"},
-            {"name": "Suyash Gupta", "image": "hod2.jpg"},
+            {"name": "Dr. Sanjeev Kumar Lambha", "image": "Screenshot 2024-12-07 224954.png"},
+            {"name": "Mr. Rishi Kumar Prajapati", "image": "Screenshot 2024-12-07 222306.png"},
             {"name": "Rahul Singh", "image": "hod3.jpg"},
             {"name": "Aditi Sharma", "image": "hod4.jpg"},
             {"name": "Ankit Raj", "image": "hod5.jpg"}
